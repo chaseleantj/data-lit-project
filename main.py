80f2a8038cec53543a5a780e74aa3e2445a311af
Chase Lean <chaseleanrocks@gmail.com>
2024-11-08 10:13:41 +0100

    Improves code structure

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ from youtube_api import YouTubeAPI
 
 dotenv.load_dotenv()
 
+keywords = ["machine learning"]
+num_results = 50
+output_path = "output.csv"
+
 client = YouTubeAPI(api_key=os.getenv("GOOGLE_API_KEY"))
-result = client.search_by_keywords(["machine learning"], limit=50)
+result = client.search_by_keywords(keywords, limit=num_results)
 
 result_df = result["video_df"]
 result_df = result_df.drop(["caption", "description"], axis=1)
-result_df.to_csv("output.csv")
+result_df.to_csv(output_path)
+
+print("Results successfully saved to", output_path)
